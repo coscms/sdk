becdc4ddb6c9941ef703eb2c7ec786fb4c55e6c1
Hank Shen <swhbox@foxmail.com>
2026-05-22 13:54:56 +0800

    update

--- sdk_payment/options.go
+++ sdk_payment/options.go
@@ -1,15 +1,23 @@
 package sdk_payment
 
 import (
+	"net/url"
+
 	"github.com/coscms/sdk/sdk_options"
 )
 
+// OptionsInterface 是 sdk_options.Options 中当前包所使用的函数的接口定义。
+type OptionsInterface interface {
+	SetGenerator(g sdk_options.URLValuesGenerator) *sdk_options.Options
+	ToURL(urlPath string, strength ...bool) (string, url.Values, error)
+}
+
 func New(typ sdk_options.Type, appInfoGetter sdk_options.AppInfoGetter, opts ...func(*sdk_options.Options)) *Options {
 	return &Options{
-		Options: sdk_options.New(typ, appInfoGetter, opts...),
+		OptionsInterface: sdk_options.New(typ, appInfoGetter, opts...),
 	}
 }
 
 type Options struct {
-	*sdk_options.Options
+	OptionsInterface
 }
